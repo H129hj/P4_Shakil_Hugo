d07c381746f388a1ad7a762cc1e9d864ccd6c9ad
Hugo Berton <hugo.berton@ynov.com>
2025-09-25 11:06:57 +0200

    feat : Initialisation P4

--- p4.go
+++ p4.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, World!")
+func InitP4() {
+	var grille [6][7]string
+	var joueur1, joueur2 string
+	joueur1 = "X"
+	joueur2 = "O"
+	var tour int
+	tour = 0
+	var colonne int
+	var ligne int
+	var gagnant bool
+	gagnant = false
 }
