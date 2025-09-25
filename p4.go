492aa4c156b8bc73f92e701dd55dc4c36f38b217
Shakil <khaldishakil@gmail.com>
2025-09-25 12:04:01 +0200

    feat: victory

--- p4.go
+++ p4.go
@@ -9,8 +9,6 @@ func InitP4() {
 	tour = 0
 	var colonne int
 	var ligne int
-	var gagnant bool
-	gagnant = false
 }
 
 func AffichageGrille(grille [6][7]string) {
@@ -19,4 +17,23 @@ func AffichageGrille(grille [6][7]string) {
             grille[i][j] = "."
         }
     }
+}
+
+func Victoire(grille [6][7]string, joueur string) {
+	var gagnant bool
+	gagnant = false
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 4; j++ {
+			if grille[i][j] == joueur && grille[i][j+1] == joueur && grille[i][j+2] == joueur && grille[i][j+3] == joueur {
+				gagnant = true
+			}
+		}
+	}
+	for j := 0; j < 7; j++ {
+		for i := 0; i < 3; i++ {
+			if grille[i][j] == joueur && grille[i+1][j] == joueur && grille[i+2][j] == joueur && grille[i+3][j] == joueur {
+				gagnant = true
+			}
+		}
+	}
 }
